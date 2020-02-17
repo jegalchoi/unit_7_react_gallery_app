c0f7eb44c086ca335d86589ca4785fdcaa840157
jegalchoi <thefreshbegal@gmail.com>
2020-02-17 10:32:54 -0800

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.']
+  },
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true
+  }
+}
