2d9b234572a29435de2d0a4692ce03bb3a1fc926
jegalchoi <thefreshbegal@gmail.com>
2020-02-19 11:38:20 -0800

    Add Stopwatch

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Stats } from './Stats'
+import Stopwatch from './Stopwatch'
 
 export default function Header({ title, players }) {
   return (
     <header>
       <Stats players={players} />
       <h1>{ title }</h1>
+      <Stopwatch />
     </header>
   )
 }
--- src/Player.jsx
+++ src/Player.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 
 import Counter from './Counter'
 
-export default function Player({ name, id, idx, score, removePlayer, ScoreChange }) {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className='remove-player' onClick={ () => removePlayer(id) }>✖</button>
-        { name }
-      </span>
-      <Counter
-        score={score}
-        idx={idx}
-        ScoreChange={ScoreChange}
-      />
-    </div>
-  )
+export default class Player extends PureComponent {
+  const { name, id, idx, score, removePlayer, ScoreChange } = this.props
+  render() {
+    return (
+      <div className="player">
+        <span className="player-name">
+          <button className='remove-player' onClick={() => removePlayer(id)}>✖</button>
+          {name}
+        </span>
+        <Counter
+          score={score}
+          idx={idx}
+          ScoreChange={ScoreChange}
+        />
+      </div>
+    )
+  }
 }
+
